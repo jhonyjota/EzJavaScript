490ccebca77d6e0aaaa2f7bfcb04604c1ad31dc1
jhonyjota <jhonysousak@hotmail.com>
2018-08-23 21:29:59 -0300

    JavaScript

---
+++ js/funcoes.js
@@ -0,0 +1,16 @@
+function trocaImagem() {
+	document.getElementById("myImage").src="img/aberta.jpg"; 				
+}
+function imagemTroca() {
+	document.getElementById("myImage").src="img/fechada.jpg";
+}	
+
+function trocaFonte() {
+	document.getElementById("demo").style.fontSize="50px";
+}	
+function ocultarElemento(){
+	document.getElementById("demo").style.display="none";
+}
+function naoOcultar(){
+	document.getElementById("demo").style.display="block";
+}
